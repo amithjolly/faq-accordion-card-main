87347a249fc42df81ad97bc8ed9ec3154ee21139
amithjolly <49495560+amithjolly@users.noreply.github.com>
2021-06-13 10:13:55 +0400

    faq accordion

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const faqs = document.querySelectorAll(".faq");
-faqs.forEach((faq) => {
-    faq.addEventListener("click", () => {
-        faq.classList.toggle("active");
-    });
-});
+const faqs = document.querySelectorAll(".question")
+faqs.forEach((question) =>
+  question.addEventListener("click", () => {
+    if (question.parentNode.classList.contains("active")) {
+      question.parentNode.classList.toggle("active");
+    } else {
+      faqs.forEach((question) =>
+        question.parentNode.classList.remove("active")
+      );
+      question.parentNode.classList.add("active");
+    }
+  }))
