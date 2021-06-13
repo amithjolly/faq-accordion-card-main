590a7dcf8a8623202e5d0698340fd3b4598dffdc
amithjolly <49495560+amithjolly@users.noreply.github.com>
2021-06-13 14:51:45 +0400

    faq accordion

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const faqs = document.querySelectorAll(".question")
-faqs.forEach((question) =>
-  question.addEventListener("click", () => {
-    if (question.parentNode.classList.contains("active")) {
-      question.parentNode.classList.toggle("active");
-    } else {
-      faqs.forEach((question) =>
-        question.parentNode.classList.remove("active")
-      );
-      question.parentNode.classList.add("active");
-    }
+this.addEventListener("DOMContentLoaded", () =>{
+  const questions = document.querySelectorAll(".question")
+  questions.forEach((question) => question.addEventListener("click", () =>{
+
+      if(question.parentNode.classList.contains("active")){
+          question.parentNode.classList.toggle("active")
+      }
+  else{
+  questions.forEach(question => question.parentNode.classList.remove("active"))
+  question.parentNode.classList.add("active")
+      }
+
   }))
+}) 
+// OR WE CAN USE THIS CODE:
+// const faqs = document.querySelectorAll(".question")
+// faqs.forEach((question) =>
+//   question.addEventListener("click", () => {
+//     question.parentNode.classList.toggle("active");
+//   }))
